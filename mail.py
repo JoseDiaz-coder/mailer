c2a1821eeff7c8d81344ea06e253fb6a4a902697
José Manuel Diaz <josema.diazh@gmail.com>
2022-02-25 23:21:06 -0600

    commit inicial

---
+++ mail.py
@@ -0,0 +1,16 @@
+from flask import (
+    Blueprint, render_template
+)
+
+from app.db import get_db
+
+bp = Blueprint('mail', __name__, url_prefix="/")
+
+@bp.route('/', methods=['GET'])
+def index():
+    db, c = get_db()
+    c.execute("SELECT * FROM email")
+    mails = c.fetchall()
+
+    return render_template('mails/index.html', mails=mails)
+
